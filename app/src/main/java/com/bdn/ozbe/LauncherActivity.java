b69e72330800ec21d539541f9eaf79ea57a498bb
Grifmaster42 <emreenes42@gmail.com>
2022-01-04 01:49:52 +0100

    V 3.2

---
+++ app/src/main/java/com/bdn/ozbe/LauncherActivity.java
@@ -0,0 +1,26 @@
+package com.bdn.ozbe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 300);
+
+
+
+    }
+}
